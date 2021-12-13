e9a175105d51eee26c679eed380d94d82b6789e8
Paulos Tadesse <paulosta952@gmail.com>
2021-12-13 18:44:01 +0300

    :rocket: clean code

--- components/Crew.vue
+++ components/Crew.vue
@@ -1,70 +1,17 @@
 <template lang="">
   <div>
     <div
-      class="h-screen w-full bg-bottom bg-cover bg-no-repeat"
-      :style="{
-        backgroundImage: `url(images/crew/background-crew-desktop.jpg)`,
-      }"
+
     >
-      <div class="relative flex justify-between pt-20 items-center">
-        <div class="ml-20">
-          <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48">
-            <g fill="none" fill-rule="evenodd">
-              <circle cx="24" cy="24" r="24" fill="#FFF" />
-              <path
-                fill="#0B0D17"
-                d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"
-              />
-            </g>
-          </svg>
-        </div>
-        <div
-          class="hidden md:block absolute border-b opacity-40 border-gray-100 w-1/3 left-72"
-        ></div>
-        <div
-          class="hidden md:block bg-white text-center xl:px-32 uppercase bg-opacity-5 text-white backdrop-filter backdrop-blur-xl"
-        >
-          <div
-            class="flex text-center pr-20 space-x-10 lg:space-x-20 pl-7 tracking-wide"
-          >
-            <div class="py-8">
-              <a href="#">
-                <NuxtLink class="space-x-1" to="/">
-                  <span class="font-bold">00</span>
-                  <span> home</span>
-                </NuxtLink>
-              </a>
-            </div>
-            <div class="py-8">
-              <a href="#">
-                <NuxtLink class="space-x-1" to="/destination">
-                  <span class="font-bold">01</span>
-                  <span> destination</span>
-                </NuxtLink>
-              </a>
-            </div>
-            <div class="border-b-4 border-white py-8">
-              <a href="#">
-                <NuxtLink class="space-x-1" to="/crew">
-                  <span class="font-bold">02</span>
-                  <span> crew</span>
-                </NuxtLink>
-              </a>
-            </div>
-            <div class="py-8">
-              <a href="#">
-                <NuxtLink class="space-x-1" to="/technology">
-                  <span class="font-bold">03</span>
-                  <span> technology</span>
-                </NuxtLink>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <img
+        class="absolute h-screen w-full bg-bottom bg-cover bg-no-repeat"
+        src="images/crew/background-crew-desktop.jpg"
+        alt=""
+        srcset=""
+      />
       <!-- destination page content -->
 
-      <div class="pt-10 md:pt-14 xl:pt-20">
+      <div class="relative top-40 pt-10 md:pt-14 xl:pt-20">
         <div
           class="flex flex-col lg:flex-row text-center lg:text-justify text-white justify-center"
         >
--- components/Destination.vue
+++ components/Destination.vue
@@ -1,70 +1,15 @@
 <template lang="">
   <div>
-    <div
-      class="h-screen w-full bg-bottom bg-cover bg-no-repeat"
-      :style="{
-        backgroundImage: `url(images/destination/background-destination-desktop.jpg)`,
-      }"
-    >
-      <div class="relative flex justify-between pt-20 items-center">
-        <div class="ml-20">
-          <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48">
-            <g fill="none" fill-rule="evenodd">
-              <circle cx="24" cy="24" r="24" fill="#FFF" />
-              <path
-                fill="#0B0D17"
-                d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"
-              />
-            </g>
-          </svg>
-        </div>
-        <div
-          class="hidden md:block absolute border-b opacity-40 border-gray-100 w-1/3 left-72"
-        ></div>
-        <div
-          class="hidden md:block bg-white text-center xl:px-32 uppercase bg-opacity-5 text-white backdrop-filter backdrop-blur-xl"
-        >
-          <div
-            class="flex text-center pr-20 space-x-10 lg:space-x-20 pl-7 tracking-wide"
-          >
-            <div class="py-8">
-              <a href="#">
-                <NuxtLink class="space-x-1" to="/">
-                  <span class="font-bold">00</span>
-                  <span> home</span>
-                </NuxtLink>
-              </a>
-            </div>
-            <div class="border-b-4 border-white py-8">
-              <a href="#">
-                <NuxtLink class="space-x-1" to="/destination">
-                  <span class="font-bold">01</span>
-                  <span> destination</span>
-                </NuxtLink>
-              </a>
-            </div>
-            <div class="py-8">
-              <a href="#">
-                <NuxtLink class="space-x-1" to="/crew">
-                  <span class="font-bold">02</span>
-                  <span> crew</span>
-                </NuxtLink>
-              </a>
-            </div>
-            <div class="py-8">
-              <a href="#">
-                <NuxtLink class="space-x-1" to="/technology">
-                  <span class="font-bold">03</span>
-                  <span> technology</span>
-                </NuxtLink>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <img
+        class="absolute h-screen w-full bg-bottom bg-cover bg-no-repeat"
+        src="images/destination/background-destination-desktop.jpg"
+        alt=""
+        srcset=""
+      />
       <!-- destination page content -->
 
-      <div class="pt-10 md:pt-14 xl:pt-24">
+      <div class="relative xl:top-40 md:pt-14 xl:pt-28">
         <div
           class="flex flex-col lg:flex-row text-center lg:text-justify text-white justify-center"
         >
@@ -119,9 +64,9 @@
                   <div class="flex flex-col divide-y space-y-14">
                     <div class="max-w-md text-gray-400">
                       Don't forget to pack your hiking boots. You'll need them
-                      to tackle Olympus Mons, the tallest planetaryplanetary mountain in
-                      our solar system, It's two and a halt times the size of
-                      Everest!
+                      to tackle Olympus Mons, the tallest planetaryplanetary
+                      mountain in our solar system, It's two and a halt times
+                      the size of Everest!
                     </div>
                     <div class="flex space-x-16 uppercase py-4">
                       <div class="flex flex-col">
