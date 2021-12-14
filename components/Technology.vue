835e9c695a87180e3f8033c603ba973f3813a3ca
Paulos Tadesse <paulosta952@gmail.com>
2021-12-14 18:41:22 +0300

    :bug: remove duplicated image path

--- components/Technology.vue
+++ components/Technology.vue
@@ -128,7 +128,7 @@ export default {
         name: 'SPACE CAPSULE',
         content:
           "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
-        imagePathPortrait: 'images/technology/image-spaceport-portrait.jpg',
+        imagePathPortrait: 'images/technology/image-space-capsule-portrait.jpg',
         imagePathLandscape:
           'images/technology/image-space-capsule-landscape.jpg',
       }
